489675f18a4a3cd662c77c4321265156d7e66ded
Roberto Capone <rcapone@gmail.com>
2024-03-20 22:33:07 -0300

    [wip] Application renamed

--- src/main/java/com/capone/decrypto/DecryptoCrudApplication.java
+++ src/main/java/com/capone/decrypto/DecryptoCrudApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiRestCrudApplication {
+public class DecryptoCrudApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApiRestCrudApplication.class, args);
+        SpringApplication.run(DecryptoCrudApplication.class, args);
     }
-
 }
